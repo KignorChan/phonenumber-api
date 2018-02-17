753db3a9f11106fa89a3607967de8010800c9adc
KignorC <kignorchan@gmail.com>
2018-02-16 23:03:54 -0500

    read pdf file solve

--- controller.js
+++ controller.js
@@ -4,6 +4,10 @@ var bodyParser = require('body-parser');
 var utils = require('./utils');                 // import lib
 var multer  = require('multer');                // handle multipart
 var textract = require('textract');
+var mime = require('mime');
+var pdfUtil = require('pdf-to-text');
+var pdfText = require('pdf-text');
+
 //var upload = multer({ dest: 'uploads/' });
 
 var storage = multer.diskStorage({
@@ -29,6 +33,7 @@ function processRequest(strArr){
   for(var i=0; i<strArr.length; i++){
     //console.log(utils.parse(strArr[i], 'CA'));
 
+    //console.log("Number Type: " + utils.getPhoneCode("6477862674"));
     try {
       // isValidNumber() throws TypeError
       if(utils.isValidNumber(utils.parse(strArr[i], 'CA'))){
@@ -37,7 +42,7 @@ function processRequest(strArr){
         flag++;
       }
     } catch (e) {
-      // console.error('catch:', e);
+      //console.error('catch:', e);
     } finally {
     }
   }
@@ -77,12 +82,24 @@ router.get('/api/phonenumbers/parse/text/:str?', function(req, res){
 
 // POST, reads binary base64 encoded txt as multipart from request body
 router.post('/api/phonenumbers/parse/file', upload.single('file'), function(req, res){
-  //console.log(req.file.originalname);
-  textract.fromFileWithPath('uploads/'+req.file.originalname, function( error, text ) {
-    //console.log(text);
+  console.log(req.file.originalname);
+  var decoded = "";
+  var fileType = mime.getType(req.file.originalname);
+  var fileExtension = mime.getExtension(fileType);
 
+  console.log(fileExtension);
+
+  if(fileExtension == "pdf"){
+    pdfText('uploads/'+req.file.originalname, function(err, chunks) {
+      if(err) throw (err);
+
+      var text="";
+      for(var i=0; i<chunks.length; i++){
+        text += chunks[i];
+      }
+      //console.log(text);
       var decoded = new Buffer(text, 'base64').toString('ascii');    // decoded str
-      //console.log(decoded);
+      //console.log(decoded);  
 
       var strArr = decoded.split('\n');
       var phones = [];
@@ -94,7 +111,26 @@ router.post('/api/phonenumbers/parse/file', upload.single('file'), function(req,
       }else{
         res.status(200).send(phones);
       }
-  })
+    })
+  }else{
+    textract.fromFileWithPath('uploads/'+req.file.originalname, function( error, text ) {
+        if(error) throw (error);
+  
+        var decoded = new Buffer(text, 'base64').toString('ascii');    // decoded str
+        console.log(decoded);
+        var strArr = decoded.split('\n');
+        var phones = [];
+        phones = processRequest(strArr);
+      
+        res.setHeader('Content-Type', 'application/json');
+        if(!phones.length){
+          res.status(400).send(phones);
+        }else{
+          res.status(200).send(phones);
+        }
+
+    })    
+  }  
 
 });
 
